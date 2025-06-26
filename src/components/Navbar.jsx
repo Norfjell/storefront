5ec9d3eb1bd4ac4e0a5aacdb47edc9712af92dc6
Liviu Bbire <l.bibire@mobizcorp.de>
2025-06-26 22:06:51 +0300

    added weather widget to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useCart } from "../context/CartContext";
 import { Link } from "react-router-dom";
+import WeatherWidget from "./WeatherWidget";
 
 export default function Navbar() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -10,10 +11,13 @@ export default function Navbar() {
   return (
     <nav className="bg-white shadow-md px-4 py-3 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
+        <div className="flex items-center gap-4">
         {/* Logo */}
         <Link to="/">
           <img src="/src/assets/react.svg" alt="Logo" className="h-8" />
         </Link>
+        <WeatherWidget />
+        </div>
 
         {/* Desktop menu */}
         <div className="hidden md:flex space-x-6 text-gray-700 font-medium">
