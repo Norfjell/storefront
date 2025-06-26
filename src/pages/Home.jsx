acffbf95ae68f112a9724b425efbcc7376002886
Liviu Bbire <l.bibire@mobizcorp.de>
2025-06-26 15:26:48 +0300

    product recommendation carousel to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import Hero from "../components/Hero";
+import ProductCarousel from "../components/ProductCarousel";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      {/* More sections (like Featured Products) can go below */}
+      <ProductCarousel />
     </div>
   );
 }
